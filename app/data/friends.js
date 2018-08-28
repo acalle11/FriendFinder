bbfef58d93abcae3b51a35d5d995a263fe5b8ea8
acalle11 <acalle11@gmail.com>
2018-08-28 13:53:36 -0500

    resubmitted

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+// save the data in your app as an array of objects. 
+//  Each of these objects should roughly follow this:
+
+var friendArray = [
+{
+  "name":"Michael Jordan",
+  "photo":"https://cdn.nba.net/nba-drupal-prod/styles/landscape/s3/2017-08/michael-jordan-news-conference-2010.jpg?itok=GgKYvMR2",
+  "scores":[
+    "2",
+    "5",
+    "1",
+    "3",
+    "5",
+    "3",
+    "5",
+    "5",
+    "3",
+    "5"
+   ]
+},
+{
+  "name":"Spongebob",
+  "photo":"https://en.wikipedia.org/wiki/SpongeBob_SquarePants_(character)#/media/File:Spongebob-squarepants.svg",
+  "scores":[
+    "5",
+    "5",
+    "5",
+    "1",
+    "1",
+    "1",
+    "5",
+    "1",
+    "1",
+    "1"
+   ]
+},
+{
+  "name":"Patrick Star",
+  "photo":"https://en.wikipedia.org/wiki/Patrick_Star#/media/File:Patrick_Star.png",
+  "scores":[
+    "1",
+    "1",
+    "4",
+    "5",
+    "5",
+    "5",
+    "3",
+    "5",
+    "2",
+    "5"
+   ]
+},
+];
+
+
+module.exports = friendArray;
